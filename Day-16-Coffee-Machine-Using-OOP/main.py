53e4da3c126d14ac69e2492553f09d1b68f94c0c
sarthakbkamble <257987214+sarthakbkamble@users.noreply.github.com>
2026-02-19 18:49:46 +0530

    Complete Day 16

--- Day-16-Coffee-Machine-Using-OOP/main.py
+++ Day-16-Coffee-Machine-Using-OOP/main.py
@@ -1,4 +1,24 @@
-from menu import Menu
+from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import art
 
+coffee_menu = Menu()
+nescafe_coffee_maker = CoffeeMaker()
+my_money_machine = MoneyMachine()
+
+coffee_machine_is_on = True
+print(art.coffee)
+print(art.coffee_text)
+while coffee_machine_is_on:
+    cust_order = input(f"What would you like? ({coffee_menu.get_items()}) :").lower()
+    if cust_order == "off":
+        coffee_machine_is_on = False
+    elif cust_order == "report":
+        nescafe_coffee_maker.report()
+        my_money_machine.report()
+    else:
+        drink = coffee_menu.find_drink(cust_order)
+        if nescafe_coffee_maker.is_resource_sufficient(drink) and my_money_machine.make_payment(drink.cost):
+            nescafe_coffee_maker.make_coffee(drink)
+    
