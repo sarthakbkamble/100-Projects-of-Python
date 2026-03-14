321e99d6b5d245205707c456b7f3742d0abdde54
sarthakbkamble <257987214+sarthakbkamble@users.noreply.github.com>
2026-03-14 23:52:10 +0530

    add more details in pong game

--- Day-22-Pong-game/main.py
+++ Day-22-Pong-game/main.py
@@ -1,29 +1,33 @@
 from turtle import Turtle, Screen
+import paddle,time
+from ball import Ball
+
 my_screen = Screen()
 my_screen.bgcolor("black")
 my_screen.setup(height=600,width=800)
 my_screen.title("Pong")
-
-paddle = Turtle()
-paddle.teleport(x=350,y=)
-paddle.penup()
-paddle.shape("square")
-paddle.shapesize(stretch_wid=5,stretch_len=1)
-paddle.color("white")
-
-
+my_screen.tracer(0)
 my_screen.listen()
-def move_up():
-    new_y = paddle.ycor()+10
-    paddle.goto(paddle.xcor(),new_y)
 
-def move_down():
-    new_y = paddle.ycor()-10
-    paddle.goto(paddle.xcor(),new_y)
-my_screen.onkeypress(key="Up",fun=move_up) 
-my_screen.onkeypress(key="Down",fun=move_down) 
+r_paddle = paddle.Paddle((350,0))
+l_paddle = paddle.Paddle((-350,0))
 
+my_ball = Ball()
 
+my_screen.onkeypress(key="Up",fun=r_paddle.move_up) 
+my_screen.onkeypress(key="Down",fun=r_paddle.move_down) 
+my_screen.onkeypress(key="w",fun=l_paddle.move_up) 
+my_screen.onkeypress(key="s",fun=l_paddle.move_down) 
+
+game_on = True
+while game_on:
+    time.sleep(0.1)
+    my_screen.update()
+    my_ball.move()
+    if my_ball.ycor()>280 or my_ball.ycor()<-280:
+        my_ball.bounce()
+    if my_ball.xcor()>380 or my_ball.xcor()<-380:
+        game_on=False
 
 
 
