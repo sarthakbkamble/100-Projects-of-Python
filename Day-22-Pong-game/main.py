ca07a3166b154df17edc6d7c056cbcad79d6777d
sarthakbkamble <257987214+sarthakbkamble@users.noreply.github.com>
2026-03-15 22:36:43 +0530

    Complete Pong game

--- Day-22-Pong-game/main.py
+++ Day-22-Pong-game/main.py
@@ -1,19 +1,22 @@
 from turtle import Turtle, Screen
 import paddle,time
 from ball import Ball
+import scoreboard 
 
 my_screen = Screen()
 my_screen.bgcolor("black")
 my_screen.setup(height=600,width=800)
 my_screen.title("Pong")
+
 my_screen.tracer(0)
-my_screen.listen()
+score_display = scoreboard.Scoreboard()
+my_ball = Ball()
+
 
 r_paddle = paddle.Paddle((350,0))
 l_paddle = paddle.Paddle((-350,0))
 
-my_ball = Ball()
-
+my_screen.listen()
 my_screen.onkeypress(key="Up",fun=r_paddle.move_up) 
 my_screen.onkeypress(key="Down",fun=r_paddle.move_down) 
 my_screen.onkeypress(key="w",fun=l_paddle.move_up) 
@@ -22,13 +25,20 @@ my_screen.onkeypress(key="s",fun=l_paddle.move_down)
 game_on = True
 while game_on:
     time.sleep(0.1)
-    my_screen.update()
+    
     my_ball.move()
-    if my_ball.ycor()>280 or my_ball.ycor()<-280:
-        my_ball.bounce()
-    if my_ball.xcor()>380 or my_ball.xcor()<-380:
-        game_on=False
 
+    if my_ball.ycor()>280 or my_ball.ycor()<-280:
+        my_ball.bounce_y()
+    if my_ball.distance(r_paddle)<50 and my_ball.xcor()>320 or my_ball.distance(l_paddle)<50 and my_ball.xcor()<-320:
+        my_ball.bounce_x()
+    if my_ball.xcor()>380 :
+        my_ball.reset_position()
+        score_display .l_point()
+    if my_ball.xcor()<-380:
+        my_ball.reset_position()
+        score_display.r_point()
+    my_screen.update()
 
 
 my_screen.exitonclick()
--- Day-22-Pong-game/paddle.py
+++ Day-22-Pong-game/paddle.py
@@ -1,4 +1,20 @@
 from turtle import Turtle
 
-class Paddle():
+class Paddle(Turtle):
+    def __init__(self,coord):
+        super().__init__()
+        self.goto(x=coord[0],y=coord[1])
+        self.penup()
+        self.shape("square")
+        self.shapesize(stretch_wid=5,stretch_len=1)
+        self.color("white")
+        
+
+    def move_up(self):
+        new_y = self.ycor()+20
+        self.goto(self.xcor(),new_y)
+
+    def move_down(self):
+        new_y = self.ycor()-20
+        self.goto(self.xcor(),new_y)
     
