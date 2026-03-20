ea4f042153b5926e33d0ae111784d678fc70c1a3
sarthakbkamble <257987214+sarthakbkamble@users.noreply.github.com>
2026-03-20 20:07:38 +0530

    Add code for creating random cars and make them move

--- Day-23/car_manager.py
+++ Day-23/car_manager.py
@@ -1,7 +1,34 @@
+from turtle import Turtle,Screen
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+class CarManager():
+    def __init__(self):
+        self.all_cars=[]
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    
+    def create_car(self):
+        """Creates car"""
+        random_chance = random.randint(1,6)
+        if random_chance == 1:
+            new_car= Turtle("square")
+            new_car.shapesize(stretch_len=2,stretch_wid=1)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y=random.randint(-230,230)
+            new_car.goto(300,random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        """used for car moving"""
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def increase_speed(self):
+        """increases car speed"""
+        self.car_speed+=MOVE_INCREMENT
 
 
-class CarManager:
-    pass
+    
