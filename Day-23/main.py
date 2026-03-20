74ee6740c30cc7de84f2722d35f280146f747b35
sarthakbkamble <257987214+sarthakbkamble@users.noreply.github.com>
2026-03-20 20:09:30 +0530

    Combine all the files in one , game complete.

--- Day-23/main.py
+++ Day-23/main.py
@@ -1,27 +1,47 @@
+# Importing Modules
 import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-
+# Initialization and basic screen setup
 screen = Screen()
 screen.tracer(0)
 screen.setup(width=600, height=600)
 scoreboard = Scoreboard()
 screen.listen()
 my_player = Player()
+carmanager = CarManager()
+
+# For player movement
 screen.onkeypress(key="Up",fun=my_player.move)
 
 
 game_is_on = True
+
+# Main game loop
 while game_is_on:
-    if my_player.ycor()>280:
-        my_player.reset_position()
-        scoreboard.update_level()
-
-
     time.sleep(0.1)
     screen.update()
 
-screen.exitonclick
+    # creates cars and moves them 
+    carmanager.create_car()
+    carmanager.move_cars()
+    
+    # Detecting collision with car
+    for car in carmanager.all_cars:
+        if car.distance(my_player)<20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detecting successful crossing 
+    if my_player.is_at_finish_line():
+        scoreboard.update_level()
+        my_player.reset_position()
+        carmanager.increase_speed()
+
+screen.exitonclick()  
+        
+
+
