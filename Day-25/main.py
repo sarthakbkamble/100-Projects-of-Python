45f1a238c5e4dd5f45ebc1f55785c14fe6233706
sarthakbkamble <257987214+sarthakbkamble@users.noreply.github.com>
2026-03-31 21:50:59 +0530

    U.S States Game complete

--- Day-25/main.py
+++ Day-25/main.py
@@ -1,24 +1,27 @@
-# with open("C:/new/100-Projects-of-Python/Day-25/weather_data.csv") as data_file:
-#     list_of_data = data_file.readlines()
-#     print(list_of_data)
+import turtle, pandas
 
-# import csv
-# with open("C:/new/100-Projects-of-Python/Day-25/weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperature=[]
-#     i = 0
-#     for row in data:
-#         if i>0:
-#             temp =int(row[1])
-#             temperature.append(temp)
-#         i+=1
-# print(temperature)
+cursor = turtle.Turtle()
+cursor.hideturtle()
+cursor.penup()
 
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "C:/new/100-Projects-of-Python/Day-25/blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+correct_states=0
 
-import pandas
-data = pandas.read_csv("C:/new/100-Projects-of-Python/Day-25/weather_data.csv")
-sum_of_temp=0
-for temp in data["temp"]:
-    sum_of_temp +=temp
-answer = sum_of_temp/len(data["temp"])
-print(answer)
+data = pandas.read_csv("C:/new/100-Projects-of-Python/Day-25/50_states.csv")
+
+while correct_states<51:
+    answer_state = (screen.textinput(title=f"{correct_states}/50 Correct States", prompt="What's the another state's name?")).title()
+    for state in data.state:
+        if answer_state == state:
+            ans_correct_row =  data[data["state"]==answer_state]
+            if answer_state == ans_correct_row.state.values[0]:
+                cursor.goto(ans_correct_row.x.values[0],ans_correct_row.y.values[0])
+                cursor.write(arg=answer_state,font=("Arial", 8, "bold"))
+                correct_states+=1
+    
+screen.exitonclick()
+
