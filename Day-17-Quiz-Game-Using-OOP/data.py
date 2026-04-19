857444b15c3d3abcb83ba53995bce35a2a5e1e16
sarthakbkamble <257987214+sarthakbkamble@users.noreply.github.com>
2026-04-19 14:00:00 +0530

    Add comments.

--- Day-17-Quiz-Game-Using-OOP/data.py
+++ Day-17-Quiz-Game-Using-OOP/data.py
@@ -1,3 +1,4 @@
+# A list of dictionaries containing trivia questions and their corresponding correct answers for the quiz
 question_data = [
     {"text": "A slug's blood is green.", "answer": "True"},
     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
