24babe4ec9248aa7efb95121a2af11a88020ac0c
sarthakbkamble <257987214+sarthakbkamble@users.noreply.github.com>
2026-04-21 14:00:00 +0530

    Add comments.

--- Day-17-Quiz-Game-Using-OOP/question_model.py
+++ Day-17-Quiz-Game-Using-OOP/question_model.py
@@ -1,4 +1,5 @@
 class Question:
     def __init__(self, question, answer):
+        # Initialize the text of the question and its corresponding correct answer
         self.question= question
         self.answer =answer
