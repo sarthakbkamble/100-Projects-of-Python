f77552c0f4b2994df643820d7cb91b0bcacaa76c
sarthakbkamble <257987214+sarthakbkamble@users.noreply.github.com>
2026-07-10 23:59:41 +0530

    Day 35 Start

---
+++ Day-35-Rain-Alert-Project/main.py
@@ -0,0 +1,38 @@
+import requests
+from twilio.rest import Client
+
+
+
+
+# MY_Lat = 16.731967
+# MY_Lon = 74.240184
+MY_Lat = 75.559693
+MY_Lon = 12.7576938
+MY_LOCATION = (MY_Lat,MY_Lon)
+api_endpoint = ""
+api_key = ""
+parameters ={
+    "lat":MY_Lat,
+    "lon":MY_Lon,
+    "appid":api_key,
+    "cnt":4,    
+}
+
+
+
+response = requests.get(url=api_endpoint,params=parameters)
+response.raise_for_status()
+data = response.json()
+for dict in data["list"]:
+    weather = {
+        "weather id":dict["weather"][0]["id"],
+        "description" : dict["weather"][0]["description"],
+    }
+    if weather["weather id"] < 700:
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            from_="whatsapp:+14155238886",
+            body="It's going to rain today. Remember to bring an umbrella ☔",
+            to="whatsapp:+919156891611"
+            )
+        print(message.status)
